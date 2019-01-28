9fd57c654979291a9261aeeb40e8b7e757a3803b
Ruslan Posevkin <rus.posevkin@gmail.com>
2019-01-28 22:16:33 +0300

    fix issue with using toWei in test cases

--- app/test/TestSupplychain.js
+++ app/test/TestSupplychain.js
@@ -14,7 +14,7 @@ contract('SupplyChain', function(accounts) {
     const originFarmLongitude = "144.341490"
     var productID = sku + upc
     const productNotes = "Best beans for Espresso"
-    const productPrice = web3.toWei(1, "ether")
+    const productPrice = web3.utils.toWei('1', "ether")
     var itemState = 0
     const distributorID = accounts[2]
     const retailerID = accounts[3]
